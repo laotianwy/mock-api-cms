b35789a69beb2ce25e11d1299e6565e69ea68aae
laotianwy <1695657342@qq.com>
2025-02-14 19:14:36 +0800

    feat：初步添加login页面

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: laotianwy 1695657342@qq.com
+ * @Date: 2025-02-14 19:13:29
+ * @LastEditors: laotianwy 1695657342@qq.com
+ * @LastEditTime: 2025-02-14 19:13:43
+ * @FilePath: /mock-api-cms/src/app/auth/login/page.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+const LoginPage = () => {
+    return <div>111</div>
+}
+
+export default LoginPage;
