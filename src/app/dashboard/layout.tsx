b5935157bb6f4641c00d9b0f8e7f79b695bf2d1b
laotianwy <1695657342@qq.com>
2025-02-15 20:12:05 +0800

    feat：添加404页面

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-02-15 18:00:01
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-15 19:31:58
+ * @LastEditTime: 2025-02-15 20:06:31
  * @FilePath: /mock-api-cms/src/app/dashboard/layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,6 +31,7 @@ const DashBoardLayout = async (props: DashBoardLayoutProps) => {
     const getUserInfoBySSR = await getUserInfo(userToken?.value);
     return (
         <HydrateUserInfo userInfo={getUserInfoBySSR}>
+            <div>dash board title</div>
             {children}
         </HydrateUserInfo>
     );
---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: laotianwy 1695657342@qq.com
+ * @Date: 2025-02-15 20:04:10
+ * @LastEditors: laotianwy 1695657342@qq.com
+ * @LastEditTime: 2025-02-15 20:04:35
+ * @FilePath: /mock-api-cms/src/app/not-found.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const NotFound = () => {
+    return <div>not found</div>;
+};
+
+export default NotFound;
