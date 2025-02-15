444ee2d2c9cb689ba329fc0e5b6864506d853797
laotianwy <1695657342@qq.com>
2025-02-15 20:25:26 +0800

    fix：修复ts类型报错

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 /*
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-02-14 19:13:29
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-15 19:41:44
+ * @LastEditTime: 2025-02-15 20:23:53
  * @FilePath: /mock-api-cms/src/app/auth/login/page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,26 +18,29 @@ import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
     const router = useRouter();
-    const [_, formAction, isPending] = useActionState((_, formData) => {
-        startTransition(async () => {
-            const res = await request.globalApi.mock.userLogin(
-                {
-                    username: 'admin',
-                    password: '123456',
-                    captcha: formData.get('captcha'),
-                },
-                {
-                    withCredentials: true,
-                },
-            );
-            jsCookie.set(GLOBAL_USER_TOKEN, res.data);
-            router.replace('/dashboard');
-        });
-    }, undefined);
+    const [, formAction, isPending] = useActionState(
+        (preState: any, formData: any) => {
+            startTransition(async () => {
+                const res = await request.globalApi.mock.userLogin(
+                    {
+                        username: 'admin',
+                        password: '123456',
+                        captcha: formData.get('captcha'),
+                    },
+                    {
+                        withCredentials: true,
+                    },
+                );
+                jsCookie.set(GLOBAL_USER_TOKEN, res.data);
+                router.replace('/dashboard');
+            });
+        },
+        undefined,
+    );
 
-    const handleSubmit = (event) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault(); // 阻止默认表单提交和重置行为
-        const formData = new FormData(event.target);
+        const formData = new FormData(event.target as HTMLFormElement);
         formAction(formData);
     };
     return (
--- src/app/dashboard/mock/page.tsx
+++ src/app/dashboard/mock/page.tsx
@@ -2,13 +2,11 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-02-15 18:03:14
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-15 20:00:59
+ * @LastEditTime: 2025-02-15 20:21:45
  * @FilePath: /mock-api-cms/src/app/dashboard/page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 'use client';
-import { atomUserInfo } from '@/atom/user';
-import { useAtomValue } from 'jotai';
 const MockPage = () => {
     return <div>mock page</div>;
 };
