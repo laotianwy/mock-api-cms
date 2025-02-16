112e88a87b496a64d793edb489e976bc5666d45c
laotianwy <1695657342@qq.com>
2025-02-17 03:59:46 +0800

    style：删除没有使用的引入

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,13 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-02-16 16:40:31
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-17 03:57:02
+ * @LastEditTime: 2025-02-17 03:59:16
  * @FilePath: /mock-api-cms/src/app/middleware.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { GLOBAL_USER_TOKEN } from './const';
-import { serviceConfig } from './config/request/swaggerServiceConfig';
 
 export const middleware = async (request: NextRequest) => {
     // 获取请求的路径
