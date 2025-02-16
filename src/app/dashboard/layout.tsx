58e634a53f4a7d07af921f9d05fb5a193666fd7f
laotianwy <1695657342@qq.com>
2025-02-16 17:41:30 +0800

    style：修改组件名字，水合ssr请求过来的数据

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,12 +2,12 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-02-15 18:00:01
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-15 20:06:31
+ * @LastEditTime: 2025-02-16 17:40:22
  * @FilePath: /mock-api-cms/src/app/dashboard/layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 'use server';
-import HydrateUserInfo from '@/component/HydrateUserInfo';
+import HydrateSSRdata from '@/component/HydrateSSRdata';
 import { request } from '@/config/request';
 import { GLOBAL_USER_TOKEN } from '@/const';
 import { cookies } from 'next/headers';
@@ -30,10 +30,10 @@ const DashBoardLayout = async (props: DashBoardLayoutProps) => {
     // 服务器端获取用户信息
     const getUserInfoBySSR = await getUserInfo(userToken?.value);
     return (
-        <HydrateUserInfo userInfo={getUserInfoBySSR}>
+        <HydrateSSRdata userInfo={getUserInfoBySSR}>
             <div>dash board title</div>
             {children}
-        </HydrateUserInfo>
+        </HydrateSSRdata>
     );
 };
 
--- src/component/HydrateSSRdata/index.tsx
+++ src/component/HydrateSSRdata/index.tsx
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-02-15 18:19:48
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-15 18:49:30
+ * @LastEditTime: 2025-02-16 17:40:03
  * @FilePath: /mock-api-cms/src/component/HydrateUserInfo/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,14 +11,14 @@ import { atomUserInfo } from '@/atom/user';
 import { UserEntity } from '@/service/globalApi';
 import { useHydrateAtoms } from 'jotai/utils';
 
-interface HydrateUserInfoProps {
+interface HydrateSSRdataProps {
     userInfo?: UserEntity;
     children: React.ReactNode;
 }
 
-const HydrateUserInfo = ({ children, userInfo }: HydrateUserInfoProps) => {
+const HydrateSSRdata = ({ children, userInfo }: HydrateSSRdataProps) => {
     useHydrateAtoms([[atomUserInfo, userInfo]]);
     return children;
 };
 
-export default HydrateUserInfo;
+export default HydrateSSRdata;
